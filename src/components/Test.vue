85d453a7aa49fbcbcb8c7e263ae118ffcc0ce911
mac <mac@macs-MacBook-Pro.local>
2019-07-18 17:10:51 +0100

    first commit

---
+++ src/components/Test.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <p v-for="car in 4" v-bind:key="car" ref="car"> I am car number {{car}}</p>
+    <button @click="submit">View refs</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  data(){
+    return {
+    }
+  },
+  methods: {
+    submit(){
+      console.log(this.$refs)
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+p , input, button{
+  font-size: 20px;
+}
+input, button{
+  font-size: 20px;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
